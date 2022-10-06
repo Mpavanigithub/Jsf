65d02a1e4f20cfec1822cf03f1c764c372aa4f23
Mpavanigithub <114507128+Mpavanigithub@users.noreply.github.com>
2022-10-06 17:43:06 +0530

    Add files via upload

---
+++ day12/JsfJdbcAgent/src/main/java/com/infinite/JsfJdbc/AgentDAO.java
@@ -0,0 +1,56 @@
+package com.infinite.JsfJdbc;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+
+
+@ManagedBean
+@SessionScoped
+public class AgentDAO {
+	
+	
+	Connection connection;
+    PreparedStatement pst;
+    
+    public String addAgent(Agent agent)
+    		throws ClassNotFoundException, SQLException {
+    connection = ConnectionHelper.getConnection();
+    String cmd = "insert into Agent(Name,City, maritalStatus,premium)"+"values(?,?,?,?)";
+    pst = connection.prepareStatement(cmd);
+    pst.setString(1,agent.getName());
+    pst.setString(2,agent.getCity());
+    pst.setInt(3,agent.getMaritalStatus());
+    pst.setDouble(4,agent.getPremium());
+    pst.executeUpdate();
+    return "Record Inserted";
+     }
+    public List<Agent> showAgent() throws ClassNotFoundException, SQLException{
+    List<Agent> agentList = new ArrayList<Agent>();
+    connection = ConnectionHelper.getConnection();
+	String cmd = "Select * from Agent";
+	pst = connection.prepareStatement(cmd);
+	ResultSet rs  =pst.executeQuery();
+	Agent agent = null;
+	while(rs.next()){
+		agent = new Agent();
+		agent.setAgentId(rs.getInt("AgentId"));
+		agent.setName(rs.getString("Name"));
+		agent.setCity(rs.getString("City"));
+		agent.setGender(rs.getString("gender"));
+		agent.setMaritalStatus(rs.getInt("MaritalStatus"));
+		agent.setPremium(rs.getDouble("Premium"));
+		agentList.add(agent);
+		
+	}
+	return agentList;
+}
+
+}
---
+++ day12/JsfValidations1/src/main/java/com/infinite/Jsf/Employ.java
@@ -0,0 +1,16 @@
+package com.infinite.Jsf;
+
+
+
+@SessionScoped
+public class Employ {
+	
+	private int empno;
+	private String name;
+	private String gender;
+	private String dept;
+	private String desig;
+	private double basic;
+	
+
+}
