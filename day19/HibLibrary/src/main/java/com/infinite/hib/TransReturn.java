abea8239b2772c00d6582b5dac92781577bfa9e8
Mpavanigithub <114507128+Mpavanigithub@users.noreply.github.com>
2022-10-19 18:07:02 +0530

    Add files via upload

---
+++ day19/HibLibrary/src/main/java/com/infinite/hib/TransReturn.java
@@ -0,0 +1,67 @@
+package com.infinite.hib;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="TransReturn")
+public class TransReturn {
+	
+	
+	@Id
+	@Column(name="tid")
+	private int tid;
+	
+	@Column(name="userName")
+	private String userName;
+	
+	@Column(name="bookId")
+	private int bookId;
+	
+	@Column(name="todate")
+	private Date todate;
+
+	public int getTid() {
+		return tid;
+	}
+
+	public void setTid(int tid) {
+		this.tid = tid;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public int getBookId() {
+		return bookId;
+	}
+
+	public void setBookId(int bookId) {
+		this.bookId = bookId;
+	}
+
+	public Date getTodate() {
+		return todate;
+	}
+
+	public void setTodate(Date todate) {
+		this.todate = todate;
+	}
+
+	@Override
+	public String toString() {
+		return "TransReturn [tid=" + tid + ", userName=" + userName + ", bookId=" + bookId + ", todate=" + todate + "]";
+	}
+
+	
+}
